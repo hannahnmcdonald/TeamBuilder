03641310f5b1759aa7da7f695b9a7b36998c86e2
Hannah McDonald <hannahnmcdonald@protonmail.com>
2021-08-26 13:47:39 -0400

    routine push. only have to do inquirer

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
+// CONSTANTS: 
 const render = require('./src/pagetemplate.js');
 const fs = require("fs");
 const path = require("path");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const inquirer = require('inquirer');
+
+const team = [];
+const idArray = [];
+
 
 // Output Directory
 const OUTPUT_DIR =  path.resolve(__dirname, "dist");
 const outputPath = path.join(OUTPUT_DIR, "index.html");
 
-const team = [
-    new Manager("Hannah", 1, "hannah@hannah.com"),
-    new Engineer("Logan", 2, "logan@logan.com"),
-    new Intern("Jasper", 3, "jasper@jasper.com"),
-];
+// const team = [
+//     new Manager("Hannah", 1, "hannah@hannah.com"),
+//     new Engineer("Logan", 2, "logan@logan.com"),
+//     new Intern("Jasper", 3, "jasper@jasper.com"),
+// ];
 
-// Builds HTML file 
-fs.writeFileSync(outputPath, render(team), "utf-8");
+function writeFile(team) {
+    // Builds HTML file 
+    fs.writeFileSync(outputPath, render(team), "utf-8");
+};
 
-// TEST: console.log(render(team));
+function init() {
+    // inquirer:
+
+  
+init();
+
+// TEST:
+// const team = [
+//     new Manager("Hannah", 1, "hannah@hannah.com"),
+//     new Engineer("Logan", 2, "logan@logan.com"),
+//     new Intern("Jasper the aloe plant", 3, "jasper@jasper.com"),
+// ]
